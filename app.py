fb48c003e94d4bb5d2ce2b7495a8755638641246
Andrew Brown <abrown95@myune.edu.au>
2022-08-17 12:15:42 +1000

    more stuff

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 
 # esp8266 ips
-URL = " http://10.0.0.56/Temperature"
+URL = " http://10.0.0.56/temperature"
 location = "Outside"
 PARAMS = {'tempOneUrl':location}
 
