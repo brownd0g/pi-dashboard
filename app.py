6f8603255b659dbfd9b07f2a0a793b6f0a23bcc2
Andrew Brown <abrown95@myune.edu.au>
2022-12-12 15:42:48 +1100

    hi

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template, make_response
 import random
 from time import time
 import json
-import RPi.GPIO as GPIO
-from w1thermsensor import W1ThermSensor
+#import RPi.GPIO as GPIO
+#from w1thermsensor import W1ThermSensor
 import requests
 
 app = Flask(__name__)
 
 # pi sensors
 #sensor = W1ThermSensor()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(21, GPIO.OUT)
 
 # esp8266 ips
-outdoorTempURL = "http://10.0.0.56/temperature"
+#fridgeEsp = 'http://...' put fridge esp8266 ip here
+doorEsp = 'http://10.0.0.117' #put door esp8266 ip here
+outdoorRearEsp = "http://10.0.0.56/"
+outdoorTempURL = outdoorRearEsp + "temperature"
 outdoorLocation = "Outside"
 PARAMS = {'tempOneUrl':outdoorLocation}
+
+global db = False
 
 
 @app.route('/')
@@ -26,41 +31,39 @@ def index():
 
 @app.route('/on')
 def on():
-    # open door
-    print("ON")
     return render_template('index.html')
 
 @app.route('/off')
 def off():
-    print("OFF")
     return render_template('index.html')
 
-@app.route('/button')
-def button():
-    return render_template('button.html')
-
-@app.route('/widget')
-def widget():
-    return render_template('widget.html')
-
-@app.route('/data')
-def data():
-
-    #fridgeTemp = sensor.get_temperature()
-    fridgeTemp = 1
-    indoorTemp = random.random() * 100
-    outdoorTemp = requests.get(url=outdoorTempURL, params=PARAMS)
-    outdoorTemp = float(outdoorTemp.content)
-
-    data = [time() * 1000, fridgeTemp, indoorTemp, outdoorTemp]
-    if fridgeTemp > 22:
-        print('Relay On')
-        GPIO.output(21, GPIO.HIGH)
-    else:
-        GPIO.output(21, GPIO.LOW)
+@app.route('/doorbell')
+def doorbell():
+    data = True
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
+
+@app.route('/data')
+def data():
+    pass
+
+    # fridgeTemp = sensor.get_temperature()
+    # fridgeTemp = 1
+    # indoorTemp = random.random() * 100
+    # outdoorTemp = requests.get(url=outdoorTempURL, params=PARAMS)
+    # outdoorTemp = float(outdoorTemp.content)
+
+    # data = [time() * 1000, fridgeTemp, indoorTemp, outdoorTemp]
+    # if fridgeTemp > 22:
+    #     print('Relay On')
+    #     GPIO.output(21, GPIO.HIGH)
+    # else:
+    #     GPIO.output(21, GPIO.LOW)
+    # response = make_response(json.dumps(data))
+    # response.content_type = 'application/json'
+    # return response
+
 ######################################
 
 
