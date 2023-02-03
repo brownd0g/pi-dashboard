284aea6b03a92de8ccf50846337663cd46f3b203
Andrew Brown <abrown95@myune.edu.au>
2023-02-03 15:47:39 +1100

    yeehaw

--- app.py
+++ app.py
@@ -77,85 +77,14 @@ def set_call_state():
     action = request.args.to_dict()
 
     if "status" in action:
+        print("setting teams status")
         ESP_TV.data["teams_status"] = action["status"]
 
     json_formatted_str = json.dumps(ESP_TV.get_json(), indent=2)
     response = make_response(json_formatted_str)
-    print(response)
+    print(response.data)
     response.content_type = 'application/json'
     return response
-
-
-# @app.route('/api/roof/lights')
-# def login():
-#     url = roof_esp + "switch"
-#     action = request.args
-#     action = action.to_dict()
-#     data_raw = requests.post(url=url, data=action,)
-#     data = data_raw.content.decode()
-#
-#     json_object = json.loads(data)
-#     json_formatted_str = json.dumps(json_object, indent=2)
-#     response = make_response(json_formatted_str)
-#
-#     response.content_type = 'application/json'
-#     return response
-#
-#
-# @app.route('/api/shelf/lights')
-# def rgb():
-#     url = roof_esp + "switch"
-#     action = request.args
-#     action = action.to_dict()
-#     col = list(action.keys())[0]
-#     val = list(action.values())[0]
-#
-#     if col == "rgb":
-#         colourDict["red"] = val
-#         colourDict["green"] = val
-#         colourDict["blue"] = val
-#     else:
-#         colourDict[col] = val
-#     print(action)
-#     #print(colourDict)
-#
-#     requests.post(url=url, data=action)
-#     txt = json.dumps(colourDict, indent=2)
-#     response = make_response(txt)
-#     response.content_type = 'application/json'
-#     return response
-#
-#
-# @app.route('/api/shelf/state')
-# def rgbState():
-#     txt = json.dumps(colourDict, indent=2)
-#     response = make_response(txt)
-#     response.content_type = 'application/json'
-#     return response
-#
-#
-# @app.route('/api/tv/lights')
-# def tv_light():
-#
-#     tv_url = tv_esp + "switch"
-#     print(tv_url)
-#     action = request.args
-#     action = action.to_dict()
-#     #print(action)
-#
-#     data_raw = requests.post(url=tv_url, data=action, )
-#     print(data_raw)
-#     data = data_raw.content.decode()
-#     print(data)
-#     json_object = json.loads(data)
-#     print(json_object)
-#
-#     json_formatted_str = json.dumps(json_object, indent=2)
-#     print(json_formatted_str)
-#     response = make_response(json_formatted_str)
-#
-#     response.content_type = 'application/json'
-#     return response
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
